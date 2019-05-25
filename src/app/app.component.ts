7ca6c163ae4d9aee5332622c91a01c6de4c3754e
asd741 <a22833593@gmail.com>
2019-05-26 01:26:09 +0800

    '設定數字上限'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,19 @@ import { log } from 'util';
 })
 export class AppComponent {
     numArr = [0];
-    inputNum:any = 1;//input現在的東西，可能是個數字，也可能是個空字符串
+    inputNum: any = 1;//input現在的東西，可能是個數字，也可能是個空字符串
     changeNum(e) {
         e = e || window.event;
-        let oldNum:number = this.inputNum || 0,//規定數字是因為下面要比大小
-            nowNum:number = parseInt(e.target.value) || 0;
-        this.inputNum = e.target.value;
-        //使用陣列渲染畫面開始
+        let oldNum: number = this.inputNum || 0,//規定數字是因為下面要比大小
+            nowNum: number = Math.min((parseInt(e.target.value) || 0), 6666);//6666是防止數字太大當機
+        if (nowNum === 6666) {
+            this.inputNum = 6666;
+            e.target.value = 6666;
+        } else {
+            this.inputNum = e.target.value;
+        }
+
+        //circle渲染開始
         if (nowNum > oldNum) {//新數據比舊數據多
             for (let i = oldNum; i < nowNum; i++) {
                 this.numArr[i] = i;
@@ -30,6 +36,6 @@ export class AppComponent {
             // console.log('刪除');
             return;
         }
-        //使用陣列渲染畫面結束
+        //circle渲染結束
     }
 }
