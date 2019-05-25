ed0a007c3eac70c907da50e5595e9d7a9acb94eb
asd741 <a22833593@gmail.com>
2019-05-26 00:41:17 +0800

    '添加註解'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { log } from 'util';
 })
 export class AppComponent {
     numArr = [0];
-    inputNum = 1;
+    inputNum:any = 1;//input現在的東西，可能是個數字，也可能是個空字符串
     changeNum(e) {
         e = e || window.event;
-        let oldNum = this.inputNum || 0,
-            nowNum = parseInt(e.target.value) || 0;
+        let oldNum:number = this.inputNum || 0,//規定數字是因為下面要比大小
+            nowNum:number = parseInt(e.target.value) || 0;
         this.inputNum = e.target.value;
         //使用陣列渲染畫面開始
         if (nowNum > oldNum) {//新數據比舊數據多
