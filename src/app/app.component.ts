af9dabb02698652dcd6539bd00bde9d3cd107ff3
asd741 <a22833593@gmail.com>
2019-05-26 00:14:13 +0800

    'ok'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
-
+import './app.component.css';
+import { log } from 'util';
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    numArr = [0];
+    inputNum = 1;
+    changeNum(e) {
+        e = e || window.event;
+        let oldNum = this.inputNum || 0, nowNum = parseInt(e.target.value);
+        //input可能是個空值，所以 || 0，target.value可能是str，str與str比大小會出現脫離常識的結果，所以parseInt
+        this.inputNum = nowNum;
 
-    I = '1'
+        //使用陣列渲染畫面開始
+        if (nowNum > oldNum) {//新數據比舊數據多
+            for (let i = oldNum; i < nowNum; i++) {
+                this.numArr[i] = i;
+            }
+            // console.log('新增');
+            return;
+        }
+        if (nowNum < oldNum) {//新數據比舊數據少
+            let vNum = oldNum - nowNum;
+            for (let i = 0; i < vNum; i++) {
+                this.numArr.pop();
+            }
+            // console.log('刪除');
+            return;
+        }
 
+
+        //使用陣列渲染畫面結束
+    }
 }
